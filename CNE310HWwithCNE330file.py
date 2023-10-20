a972d46b364c88f6125a26f0027013ad0d99d869
tsabin2023 <148308137+tsabin2023@users.noreply.github.com>
2023-10-19 18:03:01 -0700

    Create CNE310HWwithCNE330file.py

---
+++ CNE310HWwithCNE330file.py
@@ -0,0 +1,17 @@
+# Tyler Sabin
+# CNE330 – Fall Quarter 10/14/2023
+# Source Brian Huang
+# Source Zak Rubin
+
+#Your router assigns even IP addresses to Windows machines, and odd IP addresses to Linux machines.
+#Write a function, ip_to_operating_system that takes the last octet of an IP address (0-255) and returns “Linux” or
+#“Windows” If a user enters 0, report “Router”
+
+def ip_to_operating_system(octet):
+    # your code here
+    if 0 == octet:
+        return "Router"
+    elif octet % 2 == 0:
+        return "Windows"
+    else:
+        return "Linux"
